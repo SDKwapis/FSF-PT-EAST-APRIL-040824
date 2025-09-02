996c67bf8894c74cebfe197881f9d3fc6632277c
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-09-02 19:27:55 -0400

    worked on some algorithms

--- 03-JavaScript/03-Algorithms/01-arithmetic/Unsolved/arithmetic.js
+++ 03-JavaScript/03-Algorithms/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,28 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    const total = num1 + num2 
+    return total;
+};
 
-var subtract = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    const total = num1 - num2
+    return total;
+};
 
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    const total = num1 * num2
+    return total;
+};
 
-var divide = function(num1, num2) {};
+var divide = function(num1, num2) {
+    const total = num1 / num2
+    return total;
+};
+add(2,2)
+subtract(2,2)
+multiply(2,2)
+divide(2,2)
+
+
+
