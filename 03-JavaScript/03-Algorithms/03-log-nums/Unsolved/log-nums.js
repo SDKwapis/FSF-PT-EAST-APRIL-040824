fc7cc7b123676d03369477f17f907e33cb619306
sdkwapis <sdkwapis@gmail.com>
2025-09-05 12:57:24 -0400

    working on algorithms

--- 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,16 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function (num) {
+  // console.log(num)
+  for (let i = 1; i <= num; ++i) {
+    if (i <= num) {
+      console.log(i);
+    } else {
+      console.log(num);
+      return num;
+    }
+  }
+};
+
+logNums(10);
