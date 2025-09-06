d1b879a0fb5f793e21f18ade779a68fc375ebbcb
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-09-06 12:08:07 -0400

    worked on some algorithms some more

--- 04-Web-APIs/03-Algorithms/02-countdown/Unsolved/countdown.js
+++ 04-Web-APIs/03-Algorithms/02-countdown/Unsolved/countdown.js
@@ -1,4 +1,15 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-var countdown = function(num) {};
+var countdown = function(num) {
+    const backwards = []
+    for (let i = 1; i <= num; i++) {
+        backwards.push(i);
+    }
+    backwards.reverse();
+    for (let i = 0; i < backwards.length; i++) {
+        console.log(backwards[i])
+    }
+};
+
+countdown(10);
